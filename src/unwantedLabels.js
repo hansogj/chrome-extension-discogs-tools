ee84ce9a1c8766cc263404429462a3ed7093631b
Hans Ole Gjerdrum <hansogj@gmail.com>
2019-08-03 13:13:45 +0200

    added new warnable labels & added recommendable labels

--- src/unwantedLabels.js
+++ src/unwantedLabels.js
@@ -1,10 +1,51 @@
 var find = require("find-js");
-find(document, ".label_and_cat")
-  .filter(txt => txt.innerText.match(/(DOL)|(WaxTime)|(DOXY)|(Jazz Wax)|(Waxtime In Color)/))
-  .map(unwanted => {
-      unwanted.style.backgroundColor = "#e28080";
-      unwanted.style.borderRadius = "5px";
-      unwanted.style.border = "1px solid #773636";
 
-      return unwanted
-    });
+function labelAlert(labels, alertColor) {
+
+
+  find(document, ".label_and_cat")
+    .filter(labelAndCat => labels.some(label => labelAndCat.innerText.match(label)))
+    .map(match => {
+      match.style.backgroundColor = alertColor[0];
+      match.style.borderRadius = "5px";
+      match.style.border = ["1px", "solid"].concat( alertColor[1]).join(" ");
+
+      return match
+    })
+
+}
+
+labelAlert([
+  "Abkco",
+  "Abraxas",
+  "Akarma",
+  "Bad Joker",
+  "DOL",
+  "DOXY",
+  "Get Back",
+  "Jazz Wax",
+  "Lilith",
+  "Music on Vinyl",
+  "Simply Vinyl" ,
+  "Skorpio",
+  "Tapestry",
+  "Vinyl Lovers",
+  "Vinyl Magic (VM - BTF)",
+  "WaxTime",
+  "Waxtime In Color",
+  "ZYX",
+
+
+], ["#e28080", "#773636"]);
+
+
+
+labelAlert([
+  "Cisco",
+  "Mobile Fidelity",
+  "Analogue Productions",
+  "Audio Fidelity",
+  "Speaker's Corner",
+  "Moserobie Music Production",
+
+], ["#81b179", "#0e4805"]);
