51fd6d100a8f675949147a88fdfb9b0e97ca4107
Gjerdrum, Hans Ole <hans.ole.gjerdrum@storebrand.no>
2020-07-09 13:14:45 +0200

    fix typo

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,34 +1,33 @@
-import 'array.defined/lib/polyfill';
-import 'array.onempty';
-import { addToCollection } from './addToCollection';
-import { addToWantlist, removeFromWantlist } from './addToWantlist';
-import './contentscript.scss';
-import './labelAlert';
-import { uniqueRelease } from './uniqueRelease';
-import { uniqueSeller } from './uniqueSeller';
-import find from './util/find';
+import 'array.defined/lib/polyfill'
+import 'array.onempty'
+import { addToCollection } from './addToCollection'
+import { addToWantlist, removeFromWantlist } from './addToWantlist'
+import './contentscript.scss'
+import './labelAlert'
+import { uniqueRelease } from './uniqueRelease'
+import { uniqueSeller } from './uniqueSeller'
+import find from './util/find'
 
-const isThisContentscript = true;
-console.log('isThisContentscript', isThisContentscript);
-
-find('.icon.icon-envelope').forEach(
-  (elem: HTMLElement) => (elem.style.color = 'red')
-);
+find('.activity_menu_total')
+    .map((a: HTMLAnchorElement) => parseInt(a.innerText))
+    .filter((num) => num > 0)
+    .flatMap(() => find('.icon.icon-envelope'))
+    .map((elem: HTMLElement) => (elem.style.color = '#fb7a7a'))
 
 const requestMapping = {
-  uniqueSeller,
-  uniqueRelease,
-  addToWantlist,
-  removeFromWantlist,
-  addRelease: addToCollection,
-};
+    uniqueSeller,
+    uniqueRelease,
+    addToWantlist,
+    removeFromWantlist,
+    addRelease: addToCollection,
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
-  Object.keys(requestMapping)
-    .filter((action) => action === request.action)
-    .map((action) => requestMapping[action])
-    .map((action) => action(request.options))
-    .onEmpty(() => {
-      console.error('could not find any matching action', request.action);
-    })
-);
+    Object.keys(requestMapping)
+        .filter((action) => action === request.action)
+        .map((action) => requestMapping[action])
+        .map((action) => action(request.options))
+        .onEmpty(() => {
+            console.error('could not find any matching action', request.action)
+        })
+)
--- src/popup/src/constants.ts
+++ src/popup/src/constants.ts
@@ -1,43 +1,43 @@
 export interface Folder {
-  folder_id?: number;
-  name: string;
+    folder_id?: number
+    name: string
 }
 
 export const folders: Folder[] = [
-  { name: 'Add to Folder' },
-  { folder_id: 653066, name: 'LP HoG' },
-  { folder_id: 653067, name: 'CD HoG' },
-  { folder_id: 653068, name: 'CD Ingvill' },
-  { folder_id: 506841, name: 'Dumps' },
-  { folder_id: 779607, name: 'Dupletter' },
-  { folder_id: 1687712, name: 'LP Barn' },
-  { folder_id: 1687713, name: 'LP Film' },
-  { folder_id: 1193739, name: 'LP HoG - boden' },
-  { folder_id: 1516801, name: 'LP HoG - klassisk boden' },
-  { folder_id: 729999, name: 'LP Ingvill' },
-  { folder_id: 497111, name: 'LP Maus' },
-  { folder_id: 1193563, name: 'LP Maus - Boden' },
-  { folder_id: 939142, name: 'LP Olaf' },
-  { folder_id: 497112, name: 'LP Olaf Skiaker' },
-  { folder_id: 1555346, name: 'LP Olaf Skiaker Boden' },
-  { folder_id: 848675, name: 'LP Tora' },
-];
+    { name: 'Add to Folder' },
+    { folder_id: 653066, name: 'LP HoG' },
+    { folder_id: 653067, name: 'CD HoG' },
+    { folder_id: 653068, name: 'CD Ingvill' },
+    { folder_id: 506841, name: 'Dumps' },
+    { folder_id: 779607, name: 'Dupletter' },
+    { folder_id: 1687712, name: 'LP Barn' },
+    { folder_id: 1687713, name: 'LP Film' },
+    { folder_id: 1193739, name: 'LP HoG - boden' },
+    { folder_id: 1516801, name: 'LP HoG - klassisk boden' },
+    { folder_id: 729999, name: 'LP Ingvill' },
+    { folder_id: 497111, name: 'LP Maus' },
+    { folder_id: 1193563, name: 'LP Maus - Boden' },
+    { folder_id: 939142, name: 'LP Olaf' },
+    { folder_id: 497112, name: 'LP Olaf Skiaker' },
+    { folder_id: 1555346, name: 'LP Olaf Skiaker Boden' },
+    { folder_id: 848675, name: 'LP Tora' },
+]
 
 export interface Action {
-  action: string;
-  title?: string;
+    action: string
+    title?: string
 }
 
-export const uniqeSellerAction: Action = {
-  action: 'uniqeSeller',
-  title: 'Filter seller',
-};
-export const uniqeReleaseAction: Action = {
-  action: 'uniqeRelease',
-  title: 'Filter Release',
-};
-export const addReleaseAction: Action = { action: 'addRelease' };
-export const addToWantlistAction: Action = { action: 'addToWantlist' };
+export const uniqueSellerAction: Action = {
+    action: 'uniqueSeller',
+    title: 'Filter seller',
+}
+export const uniqueReleaseAction: Action = {
+    action: 'uniqueRelease',
+    title: 'Filter Release',
+}
+export const addReleaseAction: Action = { action: 'addRelease' }
+export const addToWantlistAction: Action = { action: 'addToWantlist' }
 export const removeFromWantlistAction: Action = {
-  action: 'removeFromWantlist',
-};
+    action: 'removeFromWantlist',
+}
