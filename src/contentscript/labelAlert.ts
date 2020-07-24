8ea8f73c87f203ccf026eb75a728d0e8b035ef07
Hans Ole Gjerdrum <hansogj@gmail.com>
2020-07-24 08:52:26 +0200

    added poor and good labels. Also empazing thins on own collection

--- src/contentscript/labelAlert.ts
+++ src/contentscript/labelAlert.ts
@@ -1,58 +1,68 @@
-import find from './util/find';
-import { color } from './constants';
+import find from './util/find'
+import { color } from './constants'
 
 const badLabels = [
-  'Abkco',
-  'Abraxas',
-  'Akarma',
-  'Bad Joker',
-  'DOL',
-  'DOXY',
-  'Get Back',
-  'Hi Hat',
-  'Jazz Wax',
-  'joe jackson recoords',
-  'Lilith',
-  'Music on Vinyl',
-  'Sanctuary Records',
-  'Simply Vinyl',
-  'Skorpio',
-  'Tapestry',
-  'Vinyl Lovers',
-  'Vinyl Magic (VM - BTF)',
-  'WaxTime',
-  'Waxtime In Color',
-  'ZYX',
-];
+    'Abkco',
+    'Abraxas',
+    'Akarma',
+    'Bad Joker',
+    'DOL',
+    'DOXY',
+    'Get Back',
+    'Hi Hat',
+    'Jazz Wax',
+    'joe jackson recoords',
+    'Lilith',
+    'Music on Vinyl',
+    'Sanctuary Records',
+    'Simply Vinyl',
+    'Skorpio',
+    'Tapestry',
+    'Vinyl Lovers',
+    'Vinyl Magic (VM - BTF)',
+    'Waxtime In Color',
+    'WaxTime',
+    'ZYX',
+]
 
 const goodLabels = [
-  'Cisco',
-  'Mobile Fidelity',
-  'Analogue Productions',
-  'Audio Fidelity',
-  "Speaker's Corner",
-  'Moserobie Music Production',
-  'Riverside Records',
-];
+    'ACT',
+    'AKT',
+    'Analogue Productions',
+    'Audio Fidelity',
+    'Cisco',
+    'Hubro',
+    'Jazzland',
+    'Mobile Fidelity',
+    'Moserobie Music Production',
+    'Riverside Records',
+    'Rune Grammofon',
+    'Seventh Records',
+    'Soleil Zeuhl',
+    "Speaker's Corner",
+]
 
 const labelAlert = (
-  labels: typeof badLabels | typeof goodLabels,
-  backgroundColor: string,
-  borderColor: string
-) => {
-  find('.label_and_cat')
-    .map((labelAndCat) => labelAndCat as HTMLElement)
-    .filter((labelAndCat) =>
-      labels.some((label) => labelAndCat.innerText.match(label))
-    )
-    .map((match) => {
-      // match.style = {...match.style, ...backgroundColor, {...border:  ['1px', 'solid'].concat(borderColor).join(' ')}
-      match.style.backgroundColor = backgroundColor;
-      match.style.borderRadius = '5px';
-      match.style.border = ['1px', 'solid'].concat(borderColor).join(' ');
-      return match;
-    });
-};
-
-labelAlert(badLabels, color.red.soft, color.red.strong);
-labelAlert(goodLabels, color.green.soft, color.green.strong);
+    selector: string,
+    labels: typeof badLabels | typeof goodLabels,
+    backgroundColor: string,
+    borderColor: string
+) =>
+    find(selector)
+        .map(listItem => listItem as HTMLElement)
+        .filter(listItem =>
+            labels.some(label =>
+                listItem.innerText.toLowerCase().match(label.toLowerCase())
+            )
+        )
+        .map(match => {
+            // match.style = {...match.style, ...backgroundColor, {...border:  ['1px', 'solid'].concat(borderColor).join(' ')}
+            match.style.backgroundColor = backgroundColor
+            match.style.borderRadius = '5px'
+            match.style.border = ['1px', 'solid'].concat(borderColor).join(' ')
+            return match
+        })
+;['.label_and_cat', '.collection-row'].forEach(selector => {
+    labelAlert(selector, badLabels, color.red.soft, color.red.strong)
+    labelAlert(selector, goodLabels, color.green.soft, color.green.strong)
+})
